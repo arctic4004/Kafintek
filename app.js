69c1af864ce1119809985775857387c941d16513
ebrahim Farhadi <149018717+arctic4004@users.noreply.github.com>
2026-05-06 16:18:14 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+
+const loginForm = document.querySelector("#loginForm");
+const loginInput = document.querySelector("#loginForm input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit (event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
+};
+
+loginForm.addEventListener("submit",onLoginSubmit );
